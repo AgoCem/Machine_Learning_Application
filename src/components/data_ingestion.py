591331917e3bfccb3da0d58c0d1a2f0e378f5845
Agogo94 <agostinocembalo@gmail.com>
2024-06-04 22:53:38 +0200

    Update of Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,9 +11,9 @@ from dataclasses import dataclass
 @dataclass 
 class DataIngestionConfig:
     #inputs of the data ingestion component
-    train_data_path: str=os.path.join('artifact','train.csv')
-    test_data_path: str=os.path.join('artifact','test.csv')
-    raw_data_path: str=os.path.join('artifact','raw_data.csv')
+    train_data_path: str=os.path.join('artifacts','train.csv')
+    test_data_path: str=os.path.join('artifacts','test.csv')
+    raw_data_path: str=os.path.join('artifacts','raw_data.csv')
 
 class DataIngestion:
     def __init__(self):
