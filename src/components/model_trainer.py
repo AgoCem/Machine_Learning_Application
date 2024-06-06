dd8e05e20ad30658f607280a0493cf1d0dd1e216
Agogo94 <agostinocembalo@gmail.com>
2024-06-06 23:09:35 +0200

    Update with hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -51,10 +51,74 @@ class ModelTrainer:
                 "KNeighbor" : KNeighborsRegressor(),
                 "Tree" : DecisionTreeRegressor()
             } #here i use every model that i imported, the dataset is small and
-            # this is only an example, i will not hyperparameter tuning and i only will
-            # use the default parameters
+            # this is only an example
 
-            model_report:dict=evaluate_models(X_train = X_train, y_train = y_train, X_test = X_test,y_test = y_test, models = models)
+            params={
+                "CatBoost":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "XGBoost":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "AdaBoost":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Random_Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient_Boost":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear_Reg":{},
+                "Lasso_Reg" : {
+                    'alpha': [0.01, 0.1, 1.0, 10.0, 100.0],
+                    'max_iter': [1000, 5000, 10000],
+                    'selection': ['cyclic', 'random']
+                },
+                "Ridge_Reg" : {
+                    'alpha': [0.01, 0.1, 1.0, 10.0, 100.0],
+                    'solver': ['auto', 'svd', 'cholesky', 'lsqr', 'sparse_cg', 'sag', 'saga'],
+                    'max_iter': [1000, 5000, 10000]
+                },
+                "Elastic_Net" : {
+                    'alpha': [0.01, 0.1, 1.0, 10.0, 100.0],
+                    'l1_ratio': [0.1, 0.3, 0.5, 0.7, 0.9],
+                    'max_iter': [1000, 5000, 10000]
+                },
+                "SVR" : {
+                    'kernel': ['linear', 'rbf', 'sigmoid'],
+                    'C': [0.1, 1, 10, 100],
+                    'gamma': ['scale', 'auto'],
+                    'epsilon': [0.1, 0.2, 0.5, 1.0]
+                },
+                "KNeighbor" : {
+                    'n_neighbors': [3, 5, 7, 9, 11],
+                    'weights': ['uniform', 'distance'],
+                    'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
+                    'leaf_size': [10, 30, 50],
+                    'p': [1, 2]
+                },
+                "Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+            }
+
+            model_report:dict=evaluate_models(X_train = X_train, y_train = y_train, X_test = X_test,y_test = y_test, models = models, params = params)
 
             ## to get best model score from the report
             best_model_score = max(sorted(model_report.values()))
